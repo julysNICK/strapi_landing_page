8fc960a2d27ae067587e05e9699b3576e94a8277
Julys Martins <julystube@gmail.com>
2021-04-27 16:09:33 -0300

    heroku config

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'cluster0.8vhbj.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapi_landing_pages'),
+        username: env('DATABASE_USERNAME', 'landing_pages'),
+        password: env('DATABASE_PASSWORD', 'fbOC4oaQWB4kkYsI'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a9902742-ccaa-4b19-825e-8a674884b65d'
+};
